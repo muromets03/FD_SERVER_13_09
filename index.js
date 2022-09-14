ed42cabd0df098b96a7dc60133b493dcdad7eea2
muromets03 <muromets03@gmail.com>
2022-09-14 16:55:00 +0300

    Create chunk

--- index.js
+++ index.js
@@ -1,14 +1,69 @@
-const  http = require ('http');
+const http = require("http");
+const fs = require("fs");
+const PORT = 3000;
+const users =[];
 
-const PORT =3000;
-
-const requestListener =(req, res)=>{
-console.log("req=", req)
-console.log("res=", res)
-res.end("Hi! from node!")
-}
+const requestListener = (req, res) => {
+  const {method, url} = req;
+  if(method === 'GET'){
+    if(url === '/'){
+      fs.readFile('./views/index.html', {encoding:'utf8'}, (err, data)=>{
+        if(err){
+          console.log(err)
+        }
+        res.end(data)
+      })
+      return
+    }
+    if(url === '/about'){
+      fs.readFile('./views/about.html', {encoding:'utf8'}, (err, data)=>{
+        if(err){
+          console.log(err)
+        }
+        res.end(data)
+      })
+      return
+    }
+    if(url === '/contacts'){
+      fs.readFile('./views/contacts.html', {encoding:'utf8'}, (err, data)=>{
+        if(err){
+          console.log(err)
+        }
+        res.end(data)
+    })
+    return
+    }
+  }
+  if(method==='POST'){
+    if(url==='/create-user'){
+      let jsonString = ''
+      req.on('data', chunk=>{
+      jsonString += chunk ;  
+      })
+    req.on("end", ()=>{
+      const user =JSON.parse(jsonString)
+      delete user.password;
+      user.id = Date.now()
+      users.push(user)
+      res.end(user)
+    })
+      return;
+    }
+  }
+  
+    fs.readFile('./views/404.html', {encoding:'utf8'}, (err, data)=>{
+      if(err){
+        console.log(err)
+      }
+      res.end(data)
+    })
+  
+};
 const server = http.createServer(requestListener);
-server.listen(PORT)
+server.listen(PORT);
 
 
-//console.log(server)
+
+
+
+
